577d6da814de38fa93e1449ab65ccf8827997714
nkadiadrian <adriannkadi@gmail.com>
2020-07-30 06:14:40 +0200

    Revert "ADRIAN ENTITY CREATION and LONG VS INTEGER? and UNIT?"
    This reverts commit 797a6326933e1f999d725da9ba9d02cce674b960.

--- src/main/java/com/example/demo/Entity/Category.java
+++ src/main/java/com/example/demo/Entity/Category.java
@@ -1,24 +1,14 @@
 package com.example.demo.Entity;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity // This tells Hibernate to make a table out of this class
-@Table(name = "category")
 public class Category {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer categoryID;
 
     private String name;
-
-    public Category () {
-    }
-
-    public Category(Integer categoryID, String name) {
-        this.categoryID = categoryID;
-        this.name = name;
-    }
 
     public Integer getCategoryID() {
         return categoryID;
@@ -34,14 +24,5 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return categoryID.equals(category.categoryID) &&
-                name.equals(category.name);
     }
 }
