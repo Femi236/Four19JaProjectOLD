6f5d40a444e7c1e49baec36313bd04bbe782af7c
Femi Akadiri <o.t.akadiri@student.tudelft.nl>
2020-08-01 12:33:55 +0200

    Checkstyle fix

--- src/main/java/com/example/demo/entities/Like.java
+++ src/main/java/com/example/demo/entities/Like.java
@@ -1,6 +1,10 @@
 package com.example.demo.entities;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
 @Entity
 @IdClass(LikeID.class)
--- src/main/java/com/example/demo/entities/PostCategory.java
+++ src/main/java/com/example/demo/entities/PostCategory.java
@@ -1,6 +1,10 @@
 package com.example.demo.entities;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
 @IdClass(PostCategoryID.class)
